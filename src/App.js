dd757fa72c360852247bb96d7be58b14bd4fe58a
DS <ds@DSs-MacBook-Pro.local>
2024-06-01 16:40:25 +0200

    main - Initial commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: [],
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("/api/projects")
+      .then((res) => res.json())
+      .then((result) => {
+        this.setState({
+          projects: result,
+        });
+
+        // by default, load data from the first project
+        if (result) {
+          fetch(`/api/energy?uuid=${result[0].uuid}`)
+            .then((res) => res.json())
+            .then((result) => {
+              this.setState({
+                data: result,
+              });
+            });
+        }
+      });
+  }
+
+  render() {
+    const { projects, data } = this.state;
+
+    console.log("projects", projects);
+    console.log("data", data);
+
+    const projectItems = projects.map((project) => (
+      <li key={project.name}>{project.name}</li>
+    ));
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Project list:</p>
+          <ul>{projectItems}</ul>
+          <p>Data size: {data.length}</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
