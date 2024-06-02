9207e672b125463fb5a699b20c6bd712e8ea7304
code4cake <dantevs@icloud.com>
2024-06-02 17:00:49 +0200

    main - reduit severite de qqls typescript linting rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,8 @@ module.exports = {
     // typescript-eslint
     '@typescript-eslint/no-floating-promises': 'warn',
     '@typescript-eslint/no-unsafe-assignment': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unsafe-member-access': 'warn',
     '@typescript-eslint/no-unused-vars': [
       'warn',
       {
